8a6b90045359b1be8e394befc7bc3c15a431920a
神楽坂雅詩 <cyscxchope@gmail.com>
2021-05-17 19:24:03 +0800

    支持读取YQ模板网页,及网页字符串相关转换函数
    <template id="templateName">
        <div class="demo">{{ value }}</div>
    </template>

--- yq.ts
+++ yq.ts
@@ -1,8 +1,10 @@
+import { Node } from "typescript";
+
 /**
  * 雅詩TS工具類
  * by 神楽坂雅詩
  */
- export default class YQ {
+export default class YQ {
     /**
      * 獲取 HTML DOM 物件
      * @param {string} element 物件描述（'div',','.divclass','#divid'）
@@ -43,8 +45,9 @@
      * @param {string}   url      請求網址
      * @param {object}   data     需要提交的資料
      * @param {function} callback 回撥函式
+     * @param {boolean}  async    是否使用非同步請求
      */
-    static get<T extends object>(url: string, data?: T, callback?: (data: XMLHttpRequest | null, status: number) => void): void {
+    static get<T extends object>(url: string, data?: T, callback?: (data: XMLHttpRequest | null, status: number) => void, async: boolean = true): void {
         YQ.ajax('GET', url, data, callback);
     }
     /**
@@ -52,8 +55,9 @@
      * @param {string}   url      請求網址
      * @param {object}   data     需要提交的資料
      * @param {function} callback 回撥函式
+     * @param {boolean}  async    是否使用非同步請求
      */
-    static post<T extends object>(url: string, data?: T, callback?: (data: XMLHttpRequest | null, status: number) => void): void {
+    static post<T extends object>(url: string, data?: T, callback?: (data: XMLHttpRequest | null, status: number) => void, async: boolean = true): void {
         YQ.ajax('POST', url, data, callback);
     }
     /**
@@ -62,8 +66,9 @@
      * @param {string}   url      請求網址
      * @param {object}   data     需要提交的資料
      * @param {function} callback 回撥函式
+     * @param {boolean}  async    是否使用非同步請求
      */
-    static ajax<T extends object>(type: string, url: string, data?: T, callback?: (data: XMLHttpRequest | null, status: number) => void): void {
+    static ajax<T extends object>(type: string, url: string, data?: T, callback?: (data: XMLHttpRequest | null, status: number) => void, async: boolean = true): void {
         if (url.length == 0) {
             return;
         }
@@ -207,5 +212,64 @@
             return '';
         }
         return argvs;
+    }
+    /**
+     * 將 HTML 字串轉換為 DOM
+     * @param {string} dom DOM 字串
+     * @return {NodeListOf<ChildNode>} DOM
+     */
+    static stringToDOM(dom: string): NodeListOf<ChildNode> {
+        const saveElement: HTMLDivElement = document.createElement('div');
+        saveElement.innerHTML = dom;
+        return saveElement.childNodes;
+    }
+    /**
+     * 將 DOM 轉換為 HTML 字串
+     * @param {NodeListOf<ChildNode>} node DOM
+     * @return {string} HTML 字串
+     */
+    static domToString(node: globalThis.Node): string {
+        const saveElement: HTMLDivElement = document.createElement('div');
+        saveElement.appendChild(node);
+        return saveElement.innerHTML;
+    }
+    /**
+     * 獲取兩字串之間的內容
+     * @param {string} strSource 原始字串
+     * @param {string} strStart 起始字串
+     * @param {string} strEnd 結束字串
+     * @return {string} 兩字串之間的內容
+     */
+    static stringNode(strSource: string, strStart: string, strEnd: string): string {
+        const startIndex: number = strSource.indexOf(strStart) + strStart.length;
+        const endIndex: number = strSource.indexOf(strEnd);
+        if (startIndex >= 0 && endIndex >= 0) {
+            return strSource.substring(startIndex, endIndex - startIndex);
+        }
+        return '';
+    }
+    /**
+     * 讀入模板網頁
+     * @param {string} templateFileCode 模板檔案內容
+     * @param {string} replaceList [["把哪塊","裡的內容替換成什麼"]]，塊名不能重複
+     * @param {string} templateID 取出模板檔案中的哪個模板ID
+     * @return {string} 从模板生成的 HTML
+     */
+    static loadTemplateHtml(templateFileCode: string, replaceList: string[][], templateID: string = ''): string {
+        let fStart = '';
+        const fEnd = '</template>';
+        if (templateID.length == 0) {
+            fStart = '<template>';
+        } else {
+            fStart = '<template id="' + templateID + '">';
+        }
+        let templateHTML = YQ.stringNode(templateFileCode, fStart, fEnd);
+        for (let i = 0; i < replaceList.length; i++) {
+            const replaceKV: string[] = replaceList[i];
+            const replaceK: string = '{{ ' + replaceKV[0] + ' }}';
+            const replaceV: string = replaceKV[1];
+            templateHTML = templateHTML.replace(replaceK, replaceV);
+        }
+        return templateHTML;
     }
 }
