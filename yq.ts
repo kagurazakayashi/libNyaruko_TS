730017d8140dbfd9f5f2734ec826228474642d6f
神楽坂雅詩 <cyscxchope@gmail.com>
2021-07-09 13:48:02 +0800

    加入检查数字是否在某个指定的区间内的功能

--- yq.ts
+++ yq.ts
@@ -362,4 +362,52 @@ export default class YQ {
         const prop: { isok: boolean; path: string; type: string; obj: unknown; } = YQ.getProperty(obj, property, defaultVal, showWarn);
         return prop.obj;
     }
+
+    /**
+     * 檢查數值是否在區間中
+     * @param {string | number[]} scope
+     * 區間描述文字，如 "(0,10)"
+     *     "(,100]"  : ∞ < x ≤ 100
+     *     "[0,100)" : 0 ≤ x < 100
+     * 或 區間數字陣列 [最小值,最大值]
+     * @param {number} value 要被測量的數字
+     * @param {boolean} isNewNum 返回符合範圍的 ±1 數字 (value 必須輸入整數)
+     * @return {number} isNewNum ? 符合範圍的 ±1 數字 : (-1小於 0正常 1大於)
+     */
+    static scopeCalc(scope: string | number[], value: number, isNewNum = false): number {
+        const isNumArr = scope instanceof Array;
+        const incStart: boolean = isNumArr ? true : ((scope as string).substring(0, 1) == '[');
+        const incEnd: boolean = isNumArr ? true : ((scope as string).substring(scope.length - 1) == ']');
+        const scopeArr: string[] = isNumArr ? [] : (scope as string).substring(1, scope.length - 1).split(',');
+        if (isNumArr || scopeArr[0].length > 0) {
+            const minVal: number = isNumArr ? (scope as number[])[0] : parseFloat(scopeArr[0]);
+            if (incStart) {
+                if (value < minVal) {
+                    // console.log(`数值 ${value} 小于等于 标准值 ${scope} 中的 ${minVal}`);
+                    return isNewNum ? minVal : -1;
+                }
+            } else {
+                if (value <= minVal) {
+                    // console.log(`数值 ${value} 小于 标准值 ${scope} 中的 ${minVal}`);
+                    return isNewNum ? minVal + 1 : -1;
+                }
+            }
+        }
+        if (isNumArr || scopeArr[1].length > 0) {
+            const maxVal: number = isNumArr ? (scope as number[])[1] : parseFloat(scopeArr[1]);
+            if (incEnd) {
+                if (value > maxVal) {
+                    // console.log(`数值 ${value} 大于等于 标准值 ${scope} 中的 ${maxVal}`);
+                    return isNewNum ? maxVal : 1;
+                }
+            } else {
+                if (value >= maxVal) {
+                    // console.log(`数值 ${value} 大于 标准值 ${scope} 中的 ${maxVal}`);
+                    return isNewNum ? maxVal - 1 : 1;
+                }
+            }
+        }
+        // console.log(`数值 ${value} 在此区间 ${scope}`);
+        return isNewNum ? value : 0;
+    }
 }
