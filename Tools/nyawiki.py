9b455e15b59c13eff2f47f7a19d14a9b74d87849
神楽坂雅詩 <cyscxchope@gmail.com>
2022-04-05 19:06:35 +0800

    生成 Wiki 的目录 Home.md

--- Tools/nyawiki.py
+++ Tools/nyawiki.py
@@ -13,18 +13,24 @@ fromDir = sys.argv[1]
 toDir = sys.argv[2]
 pyname: str = "["+sys.argv[0]+"] "
 opencconv = opencc.OpenCC('tw2sp.json')
+infoFile: list[str] = []
+infoInfo: list[str] = []
 
-def genMD(fromFile: str, toFile: str) -> str:
+
+def genMD(fromFile: str, toFile: str):
     f = open(fromFile, 'r', encoding="utf-8", newline="\n")
-    mode:int = -1
+    mode: int = -1
     (path, filename) = os.path.split(fromFile)
-    tmpStr:str = "# " + filename + "\n"
+    tmpStr: str = "# " + filename + "\n"
     newFile: list[str] = []
     for line in f.readlines():
         line = line.strip()
         line = opencconv.convert(line)
         if mode == -1:
-            tmpStr += "**" + line.replace("//", "") + "**\n\n"
+            info: str = line.replace("//", "").strip()
+            infoFile.append(filename)
+            infoInfo.append(info)
+            tmpStr += "**" + info + "**\n\n"
             mode = 0
         elif mode == 0:
             if "/**" in line:
@@ -48,20 +54,57 @@ def genMD(fromFile: str, toFile: str) -> str:
         if mode > 0 and "*/" in line:
             mode = 3
     f.close()
-    newFileStr:str = "\n".join(newFile)
+    newFileStr: str = "\n".join(newFile) + "\n\n[返回目录 INDEX](Home.md)\n"
     fw = open(toFile, "w", encoding="utf-8", newline="\n")
     fw.write(newFileStr)
     fw.close()
+
 
 if not os.path.isdir(fromDir):
     os.makedirs(fromDir)
 if not os.path.isdir(toDir):
     os.makedirs(toDir)
-listdir:list[str] = os.listdir(fromDir)
+listdir: list[str] = os.listdir(fromDir)
 for name in listdir:
     (file, ext) = os.path.splitext(name)
     if ext == ".ts":
-        fromPath:str = fromDir + "/" + name
-        toPath:str = toDir + "/" + file + ".md"
+        fromPath: str = fromDir + "/" + name
+        toPath: str = toDir + "/" + file + ".md"
         print(pyname, fromPath, " -> ", toPath)
         genMD(fromPath, toPath)
+
+# infoFile: list[str] = []
+# infoInfo: list[str] = []
+infoFileMax: int = 0
+infoInfoMax: int = 0
+for info in infoFile:
+    lenth: int = len(info)
+    if lenth > infoFileMax:
+        infoFileMax = lenth
+for info in infoInfo:
+    lenth: int = len(info)
+    if lenth > infoInfoMax:
+        infoInfoMax = lenth
+lines: list[str] = [
+    "# 目录 INDEX",
+    "\n请选择想要了解使用方法的文件名：\n",
+    "| FILE" + " "*(infoFileMax-4) + " | INFO" + " "*(infoInfoMax-4) + " |",
+    "| " + "-"*infoFileMax + " | " + "-"*infoInfoMax + " |",
+]
+for i in range(len(infoFile)):
+    file: str = infoFile[i]
+    info: str = infoInfo[i]
+    needSpaceFile: int = infoFileMax - len(file)
+    if needSpaceFile < 0:
+        needSpaceFile = 0
+    needSpaceInfo: int = infoInfoMax - len(info)
+    if needSpaceInfo > 0:
+        info += " " * needSpaceInfo
+    lines.append("| [" + file + "](" + file.replace("ts", "md") +
+                 ")" + " "*(needSpaceFile+1) + "| " + info + " |")
+toDir += "/Home.md"
+print(pyname, " -> ", toDir)
+fw = open(toDir, "w", encoding="utf-8", newline="\n")
+fw.write("\n".join(lines) + "\n\n此 Wiki 由脚本自动生成。\n")
+fw.close()
+print(pyname, "OK")
