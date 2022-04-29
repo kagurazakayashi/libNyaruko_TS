8524aa7668fa837b70702ebbdbfdcdcf7a2de406
神楽坂雅詩 <cyscxchope@gmail.com>
2022-04-29 17:42:16 +0800

    文件上传功能

--- nyanetwork.ts
+++ nyanetwork.ts
@@ -6,7 +6,9 @@ export default class NyaNetwork extends NyaLib {
      * 傳送 GET 請求
      * @param {string}   url      請求網址
      * @param {unknown}   data     需要提交的資料
-     * @param {function} callback 回撥函式
+     * @param {function} callback 網路請求回撥函式
+     * - {XMLHttpRequest|null} data 数据对象
+     * - {number} status HTTP状态码
      * @param {boolean}  async    是否使用非同步請求
      */
     static get<T extends unknown>(url: string, data?: T, callback?: (data: XMLHttpRequest | null, status: number) => void, async = true, isblob: boolean = false): void {
@@ -17,7 +19,9 @@ export default class NyaNetwork extends NyaLib {
      * 傳送 POST 請求
      * @param {string}   url      請求網址
      * @param {unknown}   data     需要提交的資料
-     * @param {function} callback 回撥函式
+     * @param {function} callback 網路請求回撥函式
+     * - {XMLHttpRequest|null} data 数据对象
+     * - {number} status HTTP状态码
      * @param {boolean}  async    是否使用非同步請求
      */
     static post<T extends unknown>(url: string, data?: T, callback?: (data: XMLHttpRequest | null, status: number) => void, async = true, isblob: boolean = false): void {
@@ -29,7 +33,9 @@ export default class NyaNetwork extends NyaLib {
      * @param {string}   type     請求方式
      * @param {string}   url      請求網址
      * @param {unknown}   data     需要提交的資料
-     * @param {function} callback 回撥函式
+     * @param {function} callback 網路請求回撥函式
+     * - {XMLHttpRequest|null} data 数据对象
+     * - {number} status HTTP状态码
      * @param {boolean}  async    是否使用非同步請求
      */
     static ajax<T extends unknown>(type: string, url: string, data?: T, callback?: (data: XMLHttpRequest | null, status: number) => void, async = true, isblob: boolean = false): void {
@@ -48,7 +54,7 @@ export default class NyaNetwork extends NyaLib {
         }
         const isArg = dataArr.length > 0;
         const dataStr = isArg ? dataArr.join('&') : '';
-        // this.log(`请求网址 ${url} , 数据 ${dataStr}`, this.nyaLibName);
+        // this.log(`請求網址 ${url} , 資料 ${dataStr}`, this.nyaLibName);
         const isGET = type == 'GET';
         if (isGET && isArg) {
             url += '?' + dataStr;
@@ -60,13 +66,13 @@ export default class NyaNetwork extends NyaLib {
             xhr.open(type, url, async);
         }
         xhr.onload = function () {
-            // this.log(`请求网址 ${url} 成功，返回数据 ${this.responseText}`, this.nyaLibName);
+            // this.log(`請求網址 ${url} 成功，返回資料 ${this.responseText}`, this.nyaLibName);
             if (callback) {
                 callback(this, this.status);
             }
         };
         xhr.onerror = function () {
-            // this.log(`请求网址 ${url} 失败，返回状态码 ${this.status}`, this.nyaLibName, -2);
+            // this.log(`請求網址 ${url} 失敗，返回狀態碼 ${this.status}`, this.nyaLibName, -2);
             if (callback) {
                 callback(null, this.status);
             }
@@ -81,5 +87,93 @@ export default class NyaNetwork extends NyaLib {
                 xhr.send();
             }
         }
+    }
+
+    /**
+     * 上傳一個或多個檔案，支援附加提交資料(POST)
+     * @param {string} url 接收上傳檔案的網址
+     * @param {HTMLInputElement} fileInput 網頁檔案上傳框 DOM 物件
+     * @param {isMultiple} isMultiple 是否為多個檔案上傳，文件选择
+     * @param {unknown} data 需要附加提交的引數
+     * @param {function} progress 檔案請求回撥函式
+     * - {number} status 文件上传状态 0正在上传 1上传完毕 -1取消 -2超时 -3错误
+     * - {number} value 已上传字节
+     * - {number} max 总计字节
+     * - {number} percent 已完成百分比
+     * @param {function} callback 網路請求回撥函式
+     * - {XMLHttpRequest|null} data 数据对象
+     * - {number} status HTTP状态码
+     * @param {boolean} async 是否使用非同步請求
+     * @returns {boolean} 创建上传任务是否成功
+     */
+    static uploadFile<T extends unknown>(url: string, fileInput: HTMLInputElement, isMultiple = false, data?: T, progress?: (status: number, value: number, max: number, percent: number) => void, callback?: (data: XMLHttpRequest | null, status: number) => void, async = true): boolean {
+        const files: FileList | null = fileInput.files;
+        if (files == null || files.length == 0) {
+            return false; // 沒有需要上傳的檔案
+        }
+        const form: FormData = new FormData();
+        if (data) {
+            for (const key in data) {
+                if (Object.prototype.hasOwnProperty.call(data, key)) {
+                    const element: any = data[key];
+                    form.append(key, element);
+                }
+            }
+        }
+        if (isMultiple) {
+            for (const key in files) {
+                if (Object.prototype.hasOwnProperty.call(files, key)) {
+                    const file: File = files[key];
+                    form.append('files', file, file.name);
+                }
+            }
+        } else {
+            form.append('file', files[0], files[0].name);
+        }
+        const xhr: XMLHttpRequest = new XMLHttpRequest();
+        xhr.open('post', url, async);
+        xhr.onload = function (): void {
+            // this.log(`請求網址、上傳 ${url} 成功，返回資料 ${this.responseText}`, this.nyaLibName);
+            if (callback) {
+                callback(this, this.status);
+            }
+        };
+        xhr.onerror = function (): void {
+            if (callback) {
+                callback(null, this.status);
+            }
+        };
+        xhr.upload.onloadstart = function (): void {
+            if (callback) {
+                callback(null, this.status);
+            }
+        };
+        xhr.upload.onprogress = function (evt): void {
+            if (progress) {
+                progress(0, evt.loaded, evt.total, Math.round((evt.loaded / evt.total) * 100));
+            }
+        };
+        xhr.upload.onloadend = function (evt): void {
+            if (progress) {
+                progress(1, evt.loaded, evt.total, Math.round((evt.loaded / evt.total) * 100));
+            }
+        };
+        xhr.upload.onabort = function (evt): void {
+            if (progress) {
+                progress(-1, evt.loaded, evt.total, Math.round((evt.loaded / evt.total) * 100));
+            }
+        };
+        xhr.upload.ontimeout = function (evt): void {
+            if (progress) {
+                progress(-2, evt.loaded, evt.total, Math.round((evt.loaded / evt.total) * 100));
+            }
+        };
+        xhr.upload.onerror = function (evt): void {
+            if (progress) {
+                progress(-3, evt.loaded, evt.total, Math.round((evt.loaded / evt.total) * 100));
+            }
+        };
+        xhr.send(form);
+        return true;
     }
 }
