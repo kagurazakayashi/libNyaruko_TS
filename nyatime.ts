d91ea2796d9349ef40865282664e1313c2aa4fba
神楽坂雅詩 <cyscxchope@gmail.com>
2022-05-13 18:37:27 +0800

    添加数字数组与时间字符串之间的转换功能

--- nyatime.ts
+++ nyatime.ts
@@ -3,7 +3,7 @@ import NyaStrings from './nyastrings';
 // 时间转换
 export default class NyaTime {
     /**
-     * 將時間戳轉換為時間字串（ISO 8601）
+     * 將時間戳轉換為時間字串（yyyy-mm-dd hr:mi:se）
      * @param {number|string} timestamp 時間戳（秒級或毫秒級）
      * @param {number} unitLevel 時間單位等級（預設為6年月日時分秒，小於6則從後向前移除）
      * @return 時間字串（yyyy-mm-dd hr:mi:se）
@@ -19,12 +19,12 @@ export default class NyaTime {
             ts *= 1000;
         }
         const date = new Date(ts * 1000);
-        const yyyy = unitLevel-- <= 0 ? '' : NyaStrings.addZeroToString(date.getFullYear(), 4);
-        const mm = unitLevel-- <= 0 ? '' : char[1] + NyaStrings.addZeroToString(date.getMonth() + 1);
-        const dd = unitLevel-- <= 0 ? '' : char[1] + NyaStrings.addZeroToString(date.getDate());
-        const hr = unitLevel-- <= 0 ? '' : ' ' + NyaStrings.addZeroToString(date.getHours());
-        const mi = unitLevel-- <= 0 ? '' : char[2] + NyaStrings.addZeroToString(date.getMinutes());
-        const se = unitLevel <= 0 ? '' : char[2] + NyaStrings.addZeroToString(date.getSeconds());
+        const yyyy = unitLevel-- <= 0 ? '' : NyaStrings.addZero2String(date.getFullYear(), 4);
+        const mm = unitLevel-- <= 0 ? '' : char[1] + NyaStrings.addZero2String(date.getMonth() + 1);
+        const dd = unitLevel-- <= 0 ? '' : char[1] + NyaStrings.addZero2String(date.getDate());
+        const hr = unitLevel-- <= 0 ? '' : ' ' + NyaStrings.addZero2String(date.getHours());
+        const mi = unitLevel-- <= 0 ? '' : char[2] + NyaStrings.addZero2String(date.getMinutes());
+        const se = unitLevel <= 0 ? '' : char[2] + NyaStrings.addZero2String(date.getSeconds());
         return yyyy + mm + dd + hr + mi + se;
     }
 
@@ -58,6 +58,87 @@ export default class NyaTime {
             });
         }
         return '';
+    }
+
+    /**
+     * 獲取當前日期時間的數字數值
+     * @param level 要新增的資料級別(1-7)，對應年月日時分秒毫秒
+     * 例如： 3[年月日] 6[年月日時分秒]
+     * @param date 要轉換的日期物件
+     * @return {number[]} 日期時間數值陣列
+     */
+    static nowDateTime(level: number = 6, date: Date = new Date()): number[] {
+        let dtNum: number[] = [];
+        for (let i = 0; i < level; i++) {
+            switch (i) {
+                case 0:
+                    dtNum.push(date.getFullYear());
+                    break;
+                case 1:
+                    dtNum.push(date.getMonth() + 1);
+                    break;
+                case 2:
+                    dtNum.push(date.getDate());
+                    break;
+                case 3:
+                    dtNum.push(date.getHours());
+                    break;
+                case 4:
+                    dtNum.push(date.getMinutes());
+                    break;
+                case 5:
+                    dtNum.push(date.getSeconds());
+                    break;
+                case 6:
+                    dtNum.push(date.getMilliseconds());
+                    break;
+                default:
+                    break;
+            }
+        }
+        return dtNum;
+    }
+
+    /**
+     * 時間字串（yyyy-mm-dd hr:mi:se）轉換為時間數字陣列
+     * @param {string} date 轉換為時間數字陣列
+     * @param {number} mode 0自動 1視為日期 2視為時間
+     * @return {number[]} 時間數字陣列
+     */
+    static dateTimeString2numberArray(date: string, mode: number = 0): number[] {
+        const dateTime: string[] = date.split(' ');
+        const dates: number[] = [];
+        const times: number[] = [];
+        const spchar: string[] = ['-', ':'];
+        for (const dtStr of dateTime) {
+            let dtArr: string[] = [];
+            if (mode == 1) {
+                dtArr = dtStr.split(spchar[0]);
+            } else if (mode == 2) {
+                dtArr = dtStr.split(spchar[1]);
+            } else {
+                dtArr = dtStr.split(spchar[0]);
+                if (dtArr.length <= 1) {
+                    dtArr = dtStr.split(spchar[1]);
+                    if (dtArr.length <= 1) {
+                        break;
+                    } else {
+                        mode = 2;
+                    }
+                } else {
+                    mode = 1;
+                }
+            }
+            for (const dt of dtArr) {
+                const dtNum = parseInt(dt);
+                if (mode == 1) {
+                    dates.push(dtNum);
+                } else if (mode == 2) {
+                    times.push(dtNum);
+                }
+            }
+        }
+        return dates.concat(times);
     }
 
     /**
