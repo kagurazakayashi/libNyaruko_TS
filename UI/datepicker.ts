c4f21d96d61df28e9637438048ddbcefa5a5df26
神楽坂雅詩 <cyscxchope@gmail.com>
2022-05-15 18:45:13 +0800

    日历小组件可以关联input控件

--- UI/datepicker.ts
+++ UI/datepicker.ts
@@ -4,22 +4,27 @@
 // https://codepen.io/dffzmxj/pen/RwVyOOv
 // https://zhuanlan.zhihu.com/p/57043693
 
+import NyaAs from '../nyaas';
 import NyaDom from '../nyadom';
 import NyaEvent, { NyaEventListener } from '../nyaevent';
+import NyaStrings from '../nyastrings';
+import NyaTime from '../nyatime';
 
 export default class NyaDatePicker {
     stack: any[] = [];
     wrapper = document.createElement('div');
-    domBox: HTMLElement;
+    domBox: HTMLDivElement | HTMLSpanElement;
     dom: HTMLDivElement = document.createElement('div');
-    nowDateTime: number[];
+    domInput: HTMLInputElement;
+    nowDateTime: number[] = [];
     currMonth: HTMLSpanElement = document.createElement('span');
     datepicker: HTMLDivElement = document.createElement('div');
-    nowSelectStart: number[];
+    nowSelectStart: number[] = [];
     eventDay: NyaEventListener[] = [];
     events: NyaEventListener[] = [];
     selectDate: HTMLSpanElement = document.createElement('span');
     selectedColor: string = '#87CECB';
+    inputEvent: NyaEventListener | null = null;
 
     langEngWeekday: string[] = 'Sun,Mon,Tue,Wed,Thu,Fri,Sat'.split(',');
     langChsWeekday: string[] = '日一二三四五六'.split('');
@@ -27,29 +32,53 @@ export default class NyaDatePicker {
     langEngMonths: string[] = 'January,February,March,April,May,June,July,August,September,October,November,December'.split(',');
     langChsMonths: string[] = '一,二,三,四,五,六,七,八,九,十,十一,十二'.split(',');
 
-    constructor(codeDir: string, toDOM: HTMLElement) {
+    constructor(inputDOM: HTMLInputElement, addToDOM: HTMLDivElement | HTMLSpanElement) {
         for (let i = 0; i < this.langChsMonths.length; i++) {
             this.langChsMonths[i] += '月';
         }
-        const today = new Date();
-        this.nowDateTime = [today.getFullYear(), today.getMonth() + 1, today.getDate(), today.getHours(), today.getMinutes(), today.getSeconds()];
+        this.domBox = addToDOM;
+        this.domInput = inputDOM;
+        this.inputEventOn(true);
+    }
+
+    inputEventOn(isEvent: boolean) {
+        console.log(isEvent);
+        if (isEvent) {
+            console.log('!!addEventListener', this.domInput);
+            this.inputEvent = NyaEvent.addEventListener(this.domInput, () => {
+                this.show();
+                this.inputEventOn(false);
+            });
+            console.log(this.inputEvent);
+        } else {
+            if (this.inputEvent) {
+                NyaEvent.removeEventListener(this.inputEvent);
+            }
+        }
+    }
+
+    show() {
+        const inputVal: string = this.domInput.value;
+        if (inputVal.length == 0 || isNaN(Date.parse(inputVal))) {
+            this.nowDateTime = NyaTime.nowDateTime(3);
+            this.domInput.value = NyaStrings.numberArray2string(this.nowDateTime, '-', 2);
+        } else {
+            this.nowDateTime = NyaStrings.string2intArray(inputVal, '-', 3);
+        }
         this.nowSelectStart = [...this.nowDateTime];
-        this.domBox = toDOM;
-        // 最外層區域
-        this.dom = document.createElement('div');
-        this.dom.style.width = '240px';
-        this.dom.style.fontSize = '16px';
-        this.dom.style.color = '#666666';
-        this.dom.style.boxShadow = '2px 2px 8px 2px rgba(128, 128, 128, 0.3)';
-        this.domBox.appendChild(this.dom);
-        this.buildUi(this.nowDateTime[0], this.nowDateTime[1]);
+        this.buildUi();
     }
 
     destructor() {
         NyaEvent.removeEventListeners(this.events);
         NyaEvent.removeEventListeners(this.eventDay);
+        // this.inputEventOn(false);
+        this.events = [];
         this.eventDay = [];
         this.datepicker.innerHTML = '';
+        this.dom.innerHTML = '';
+        this.domBox.innerHTML = '';
+        this.currMonth = NyaAs.span();
     }
 
     getMonthDate(
@@ -125,95 +154,97 @@ export default class NyaDatePicker {
         };
     }
 
-    buildUi(year: number = -1, month: number = -1) {
-        this.dom.innerHTML = '';
-        const div = 'div';
-        const span = 'span';
-        const button = 'button';
-        const select = 'select';
-        const option = 'option';
-        const styleHeaderFooter = (d: HTMLElement) => {
-            d.style.padding = '0 20px';
+    buildUi(year: number = this.nowDateTime[0], month: number = this.nowDateTime[1]) {
+        const none: string = 'none';
+        const styleHeaderFooter: (d: HTMLElement) => void = (d: HTMLElement) => {
             d.style.height = '50px';
             d.style.lineHeight = '50px';
             d.style.background = '#f0f0f0';
             d.style.borderBottom = '1px solid #cccccc';
         };
-        const styleButton = (d: HTMLElement) => {
+        const styleButton: (d: HTMLElement) => void = (d: HTMLElement) => {
             d.style.fontFamily = 'serif';
             d.style.fontSize = '20px';
-            d.style.width = '20px';
+            d.style.width = '50px';
             d.style.height = '50px';
             d.style.lineHeight = '50px';
             d.style.color = '#1abc9c';
             d.style.textAlign = 'center';
             d.style.cursor = 'pointer';
-            d.style.textDecoration = 'none';
+            d.style.textDecoration = none;
+            d.style.border = none;
         };
-        const styleSelect = (d: HTMLElement) => {
-            d.style.border = 'none';
+        const styleSelect: (d: HTMLElement) => void = (d: HTMLElement) => {
+            d.style.border = none;
             d.style.backgroundColor = 'transparent';
             d.style.textAlign = 'right';
         };
-        const uidatepickerbtn = 'ui-datepicker-btn ';
+        // 最外層區域
+        this.dom ??= document.createElement('div');
+        this.dom.innerHTML = '';
+        this.dom.style.width = '240px';
+        this.dom.style.fontSize = '16px';
+        this.dom.style.color = '#666666';
+        this.dom.style.backgroundColor = '#FFF';
+        this.dom.style.boxShadow = '2px 2px 8px 2px rgba(128, 128, 128, 0.3)';
+        this.dom.style.position = 'absolute';
+        this.dom.style.zIndex = '99999';
+        this.domBox.appendChild(this.dom);
         // 頭部區域
-        const headerDiv: HTMLDivElement = document.createElement(div);
+        const headerDiv: HTMLDivElement = NyaAs.div();
         headerDiv.style.textAlign = 'center';
         styleHeaderFooter(headerDiv);
         // 按鈕：上個月
-        const monPrev: HTMLButtonElement = document.createElement(button);
+        const monPrev: HTMLButtonElement = NyaAs.button();
         styleButton(monPrev);
         monPrev.style.float = 'left';
         monPrev.innerHTML = '&lt;';
         headerDiv.appendChild(monPrev);
-        const eventMonPrev: NyaEventListener | null = NyaEvent.addEventListener(monPrev, '', (event: Event) => {
+        const eventMonPrev: NyaEventListener | null = NyaEvent.addEventListener(monPrev, (event: Event) => {
             this.switchMonth(false);
         });
         if (eventMonPrev) this.events.push(eventMonPrev);
         // 按鈕：下個月
-        const monNext: HTMLButtonElement = document.createElement(button);
+        const monNext: HTMLButtonElement = NyaAs.button();
         styleButton(monNext);
         monNext.style.float = 'right';
-        monNext.className = uidatepickerbtn + 'ui-datepicker-next-btn';
         monNext.innerHTML = '&gt;';
         headerDiv.appendChild(monNext);
-        const eventMonNext: NyaEventListener | null = NyaEvent.addEventListener(monNext, '', (event: Event) => {
+        const eventMonNext: NyaEventListener | null = NyaEvent.addEventListener(monNext, (event: Event) => {
             this.switchMonth(true);
         });
         if (eventMonNext) this.events.push(eventMonNext);
-        // 年月選擇器
-        this.currMonth.className = 'ui-datepicker-curr-month';
         // 年份選擇器
-        const selectYear: HTMLSelectElement = document.createElement(select);
+        const selectYear: HTMLSelectElement = NyaAs.select();
         styleSelect(selectYear);
         selectYear.title = 'Year';
         let maxYear = this.nowDateTime[0] + 100;
         for (let i = 1970; i <= maxYear; i++) {
-            const selectItem: HTMLOptionElement = document.createElement(option);
+            const selectItem: HTMLOptionElement = NyaAs.option();
             selectItem.value = i.toString();
             selectItem.innerText = selectItem.value;
             selectYear.appendChild(selectItem);
         }
-        const eventYear: NyaEventListener | null = NyaEvent.addEventListener(selectYear, '', (event: Event) => {
+        const eventYear: NyaEventListener | null = NyaEvent.addEventListener(selectYear, (event: Event) => {
             this.nowDateTime[0] = parseInt(selectYear.value);
             this.buildUiCal();
         });
         if (eventYear) this.events.push(eventYear);
         this.currMonth.appendChild(selectYear);
-        const currMonthSpan: HTMLSpanElement = document.createElement(span);
+        const currMonthSpan: HTMLSpanElement = NyaAs.span();
         currMonthSpan.innerText = ' / ';
         this.currMonth.appendChild(currMonthSpan);
         // 月份選擇器
-        const selectMonth: HTMLSelectElement = document.createElement(select);
+        const selectMonth: HTMLSelectElement = NyaAs.select();
         styleSelect(selectMonth);
         selectMonth.title = 'Month';
         for (let i = 1; i <= 12; i++) {
-            const selectItem: HTMLOptionElement = document.createElement(option);
+            const selectItem: HTMLOptionElement = NyaAs.option();
             selectItem.value = i.toString();
             selectItem.innerText = selectItem.value;
             selectMonth.appendChild(selectItem);
         }
-        const eventMonth: NyaEventListener | null = NyaEvent.addEventListener(selectMonth, '', (event: Event) => {
+        const eventMonth: NyaEventListener | null = NyaEvent.addEventListener(selectMonth, (event: Event) => {
             this.nowDateTime[1] = parseInt(selectMonth.value);
             this.buildUiCal();
         });
@@ -225,30 +256,39 @@ export default class NyaDatePicker {
         this.buildUiCal(year, month);
         this.dom.appendChild(this.datepicker);
         // 底部操作區
-        const footerDiv = document.createElement(div);
+        const footerDiv = NyaAs.div();
         footerDiv.style.textAlign = 'right';
         styleHeaderFooter(footerDiv);
         // 按鈕：確定
-        const btnOK: HTMLButtonElement = document.createElement(button);
+        const btnOK: HTMLButtonElement = NyaAs.button();
         styleButton(btnOK);
         btnOK.style.paddingRight = '15px';
         btnOK.innerHTML = '✓';
         headerDiv.appendChild(btnOK);
-        const footerDivButtons: HTMLSpanElement = document.createElement(span);
-        const eventBtnOK: NyaEventListener | null = NyaEvent.addEventListener(btnOK, '', (event: Event) => {});
+        const footerDivButtons: HTMLSpanElement = NyaAs.span();
+        const eventBtnOK: NyaEventListener | null = NyaEvent.addEventListener(btnOK, (event: Event) => {
+            console.log('this.nowDateTime', this.nowDateTime);
+            this.domInput.value = this.selectDate.innerText;
+            this.destructor();
+            this.inputEventOn(true);
+        });
         if (eventBtnOK) this.events.push(eventBtnOK);
         footerDivButtons.appendChild(btnOK);
         // 按鈕：取消
-        const btnCancel: HTMLButtonElement = document.createElement(button);
+        const btnCancel: HTMLButtonElement = NyaAs.button();
         styleButton(btnCancel);
         btnCancel.innerHTML = '×';
-        const eventBtnCancel: NyaEventListener | null = NyaEvent.addEventListener(btnCancel, '', (event: Event) => {});
+        const eventBtnCancel: NyaEventListener | null = NyaEvent.addEventListener(btnCancel, (event: Event) => {
+            this.destructor();
+            this.inputEventOn(true);
+        });
         if (eventBtnCancel) this.events.push(eventBtnCancel);
         footerDivButtons.appendChild(btnCancel);
         footerDiv.appendChild(footerDivButtons);
         // 底部日期显示
         this.selectedDate();
         this.selectDate.style.float = 'left';
+        this.selectDate.style.paddingLeft = '10px';
         footerDiv.appendChild(this.selectDate);
         this.dom.appendChild(footerDiv);
     }
@@ -333,7 +373,7 @@ export default class NyaDatePicker {
                 console.log('this.nowSelectStart', this.nowSelectStart);
                 td.style.backgroundColor = this.selectedColor;
             }
-            const event = NyaEvent.addEventListener(td, '', (event: Event) => {
+            const event = NyaEvent.addEventListener(td, (event: Event) => {
                 this.nowSelectStart[0] = titleArr[0];
                 this.nowSelectStart[1] = titleArr[1];
                 this.nowSelectStart[2] = titleArr[2];
@@ -343,7 +383,9 @@ export default class NyaDatePicker {
                 td.style.backgroundColor = this.selectedColor;
                 this.selectedDate();
             });
-            if (event) this.eventDay.push(event);
+            if (event) {
+                this.eventDay.push(event);
+            }
             tr.appendChild(td);
             // </日期數字>
             if (i % 7 === 6) {
