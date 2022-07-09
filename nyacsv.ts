77bbcb5d8fea718e3479021bca51d91c5f78c8bc
神楽坂雅詩 <cyscxchope@gmail.com>
2022-07-09 18:11:20 +0800

    解析 CSV 格式表格内容
    到二维数组

--- nyacsv.ts
+++ nyacsv.ts
@@ -1,71 +1,72 @@
-// CSV 檔案解析
+// CSV 表格处理
+
 export default class NyaCSV {
-  /**
-   * CSV 檔案解析
-   * @param {string} csv 要解析的 CSV 檔案內容
-   * @returns {string[][]} 解析後的二維陣列
-   */
-  static parse(csv: string): string[][] {
-    const rows: string[] = csv.split("\n");
-    let returnData: string[][] = [];
-    for (const e of rows) {
-      const preColumn: string[] = e.split(",");
-      let lineData: string[] = [];
-      for (let i = 0; i < preColumn.length; i++) {
-        const be: string = preColumn[i];
-        const quotationMarks: string[] = be.split('"');
-        if (quotationMarks.length > 2) {
-          const okData: string[] = [];
-          let isnull: boolean = false;
-          if (quotationMarks[0].length != 0) {
-            for (let j = 0; j < quotationMarks.length; j++) {
-              const de: string = quotationMarks[j];
-              if (j < quotationMarks.length - 1) {
-                if (de == "" && !isnull) {
-                  isnull = true;
-                  okData.push(de);
-                  continue;
-                } else if (de != "") {
-                  okData.push(de);
+    /**
+     * 解析 CSV 格式字串
+     * @param {string} csv CSV 格式字串
+     * @return {string[][]} 表格資料的二維陣列
+     */
+    static parse(csv: string): string[][] {
+        const aa = csv.split('\n');
+        let ee: string[][] = [];
+        aa.forEach((e) => {
+            const bb = e.split(',');
+            let cc = [];
+            for (let i = 0; i < bb.length; i++) {
+                const be = bb[i];
+                const dd = be.split('"');
+                if (dd.length > 2) {
+                    const zz = [];
+                    let isnull = false;
+                    if (dd[0].length != 0) {
+                        for (let j = 0; j < dd.length; j++) {
+                            const de = dd[j];
+                            if (j < dd.length - 1) {
+                                if (de == '' && !isnull) {
+                                    isnull = true;
+                                    zz.push(de);
+                                    continue;
+                                } else if (de != '') {
+                                    zz.push(de);
+                                }
+                            }
+                            if (isnull) {
+                                isnull = false;
+                            }
+                        }
+                        const zzstr = zz.join('"');
+                        cc[i - 1] += ',' + zzstr;
+                    } else {
+                        for (let j = 0; j < dd.length; j++) {
+                            const de = dd[j];
+                            if (j > 0) {
+                                if (de == '' && !isnull) {
+                                    isnull = true;
+                                    zz.push(de);
+                                    continue;
+                                } else if (de != '') {
+                                    zz.push(de);
+                                }
+                            }
+                            if (isnull) {
+                                isnull = false;
+                            }
+                        }
+                        const zzstr = zz.join('"');
+                        cc.push(zzstr);
+                    }
+                } else if (dd.length == 2) {
+                    if (dd[0].length != 0) {
+                        cc[i - 1] += ',' + dd[0];
+                    } else {
+                        cc.push(dd[1]);
+                    }
+                } else {
+                    cc.push(be);
                 }
-              }
-              if (isnull) {
-                isnull = false;
-              }
             }
-            const okDataStr: string = okData.join('"');
-            lineData[i - 1] += "," + okDataStr;
-          } else {
-            for (let j = 0; j < quotationMarks.length; j++) {
-              const de: string = quotationMarks[j];
-              if (j > 0) {
-                if (de == "" && !isnull) {
-                  isnull = true;
-                  okData.push(de);
-                  continue;
-                } else if (de != "") {
-                  okData.push(de);
-                }
-              }
-              if (isnull) {
-                isnull = false;
-              }
-            }
-            const okDatastr: string = okData.join('"');
-            lineData.push(okDatastr);
-          }
-        } else if (quotationMarks.length == 2) {
-          if (quotationMarks[0].length != 0) {
-            lineData[i - 1] += "," + quotationMarks[0];
-          } else {
-            lineData.push(quotationMarks[1]);
-          }
-        } else {
-          lineData.push(be);
-        }
-      }
-      returnData.push(lineData);
+            ee.push(cc);
+        });
+        return ee;
     }
-    return returnData;
-  }
 }
