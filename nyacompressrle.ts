03dd2d2252c19d2441bd8835f14a6ff52f03df5a
神楽坂雅詩 <cyscxchope@gmail.com>
2022-07-22 18:50:02 +0800

    RLE压缩支持任意长度的重复字符
    但要求被压缩的内容里没有数字

--- nyacompressrle.ts
+++ nyacompressrle.ts
@@ -1,19 +1,18 @@
 // 壓縮: 行程長度壓縮(RLE)
 // 記錄每個連續字元的出現次數來壓縮，適用於連續同一個字元出現次數較多的字串，否則會造成壓縮後反而更大。
-// DEMO: abbcccdddd55555666666 -> 1a2b3c4d5566
 
 import NyaStrings from './nyastrings';
 
 export default class NyaCompressRLE {
     /**
-     * 壓縮字串
+     * 壓縮字串（重複字元長度最大為36，移位-1以利用0，可自動拆分)
      * @param {string} str 原文
      * @return {string} 壓縮後的字串
      */
-    static compression(str: string): string {
+    static compression(str: string, base = 64, shift = -1): string {
         const result: string[] = [];
         let nowChar: string = '';
-        let nowLen: number = 1;
+        let nowLen: number = 1 + shift;
         for (const char of str) {
             if (nowChar.length == 0) {
                 nowChar = char;
@@ -22,7 +21,7 @@ export default class NyaCompressRLE {
                     nowLen++;
                 } else {
                     result.push(nowLen.toString(), nowChar);
-                    nowLen = 1;
+                    nowLen = 1 + shift;
                     nowChar = char;
                 }
             }
@@ -48,6 +47,68 @@ export default class NyaCompressRLE {
             isLen = !isLen;
         }
         return result.join('');
+    }
+
+    /**
+     * 壓縮字串(支援多位長度數字,但原文不能包含數字)
+     * @param {string} str 原文
+     * @return {string} 壓縮後的字串
+     */
+    static compressionEx(str: string): string {
+        let pre: string = str[0];
+        let res: string = '';
+        let curNum: number = 1;
+        for (let i = 1; i < str.length; i++) {
+            const char = str[i];
+            if (!isNaN(parseInt(char))) {
+                console.error('sc', char);
+                return '';
+            }
+            if (char === pre) {
+                curNum++;
+            } else {
+                res += pre;
+                res += curNum;
+                pre = char;
+                curNum = 1;
+            }
+        }
+        res += pre;
+        res += curNum;
+        if (res.length > str.length) {
+            console.warn('sc', str.length, res.length);
+        } else {
+            console.log('sc', str.length, res.length);
+        }
+        return res;
+    }
+
+    /**
+     * 解壓縮字串(支援多位長度數字,但原文不能包含數字)
+     * @param {string} str 壓縮後的字串
+     * @return {string} 原文
+     */
+    static decompressionEx(str: string): string {
+        let result: string = '';
+        const strArr: string[] = (str + 'X').split('');
+        let nowchar = '';
+        let nownum = '';
+        for (let i = 0; i < strArr.length; i++) {
+            const char: string = strArr[i];
+            const isNumber = !isNaN(parseInt(char));
+            if (isNumber) {
+                nownum += char;
+            } else {
+                if (nownum.length > 0) {
+                    for (let j = 0; j < parseInt(nownum); j++) {
+                        result += nowchar;
+                    }
+                    nownum = '';
+                }
+                nowchar = char;
+            }
+        }
+        return result;
     }
 
     /**
