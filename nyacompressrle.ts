2b61efc2dcade00db9bbc860a869bbaa49454e62
神楽坂雅詩 <cyscxchope@gmail.com>
2022-07-24 18:46:34 +0800

    增强RLE压缩的压缩比和兼容性
    同时支持超长相同字符宽度和数字压缩

--- nyacompressrle.ts
+++ nyacompressrle.ts
@@ -1,46 +1,66 @@
 // 壓縮: 行程長度壓縮(RLE)
 // 記錄每個連續字元的出現次數來壓縮，適用於連續同一個字元出現次數較多的字串，否則會造成壓縮後反而更大。
+// DEMO: abbcccdddd55555666666 -> 1a2b3c4d5566
 
+import NyaCalc from './nyacalc';
 import NyaStrings from './nyastrings';
 
 export default class NyaCompressRLE {
     /**
-     * 壓縮字串（重複字元長度最大為36，移位-1以利用0，可自動拆分)
+     * 壓縮字串
      * @param {string} str 原文
+     * @param {number} base 按進位制壓縮位數（可以利用的字元數，選項：10,16,32,62,64,96）
+     * @param {number} offset 計數偏移（預設-1，可以利用字元0）
      * @return {string} 壓縮後的字串
      */
-    static compression(str: string, base = 64, shift = -1): string {
+    static compression(str: string, base = 64, offset = -1): string {
         const result: string[] = [];
         let nowChar: string = '';
-        let nowLen: number = 1 + shift;
+        let nowLen: number = 1 + offset;
+        let nextLen: number = 0;
+        let nowLenBase: string = '';
+        let nextLenBase: string = '';
         for (const char of str) {
             if (nowChar.length == 0) {
                 nowChar = char;
             } else {
+                nowLenBase = NyaCalc.baseChar(nowLen, 10, base);
                 if (char == nowChar) {
+                    nextLen = nowLen + 1;
+                    nextLenBase = NyaCalc.baseChar(nextLen, 10, base);
+                    if (nextLenBase.length > 1) {
+                        result.push(nowLenBase, nowChar);
+                        nowLen = 0 + offset;
+                        nowChar = char;
+                    }
                     nowLen++;
                 } else {
-                    result.push(nowLen.toString(), nowChar);
-                    nowLen = 1 + shift;
+                    result.push(nowLenBase, nowChar);
+                    nowLen = 1 + offset;
                     nowChar = char;
                 }
             }
         }
-        return result.join('') + nowLen.toString() + nowChar;
+        nowLenBase = NyaCalc.baseChar(nowLen, 10, base);
+        return result.join('') + nowLenBase + nowChar;
     }
 
     /**
      * 解壓縮字串
      * @param {string} str 壓縮後的字串
+     * @param {number} base 按進位制壓縮位數（可以利用的字元數，選項：10,16,32,62,64,96）
+     * @param {number} offset 計數偏移（預設-1，可以利用字元0）
      * @return {string} 原文
      */
-    static decompression(str: string): string {
+    static decompression(str: string, base = 64, offset = -1): string {
         const result: string[] = [];
         let isLen: boolean = true;
         let len: number = 0;
+        let char10:string='';
         for (const char of str) {
             if (isLen) {
-                len = parseInt(char);
+                char10 = NyaCalc.baseChar(char,base,10);
+                len = parseInt(char10) + offset * -1;
             } else {
                 result.push(NyaStrings.repeat(char, len));
             }
@@ -49,86 +69,22 @@ export default class NyaCompressRLE {
         return result.join('');
     }
 
-    /**
-     * 壓縮字串(支援多位長度數字,但原文不能包含數字)
-     * @param {string} str 原文
-     * @return {string} 壓縮後的字串
-     */
-    static compressionEx(str: string): string {
-        let pre: string = str[0];
-        let res: string = '';
-        let curNum: number = 1;
-        for (let i = 1; i < str.length; i++) {
-            const char = str[i];
-            if (!isNaN(parseInt(char))) {
-                console.error('sc', char);
-                return '';
-            }
-            if (char === pre) {
-                curNum++;
-            } else {
-                res += pre;
-                res += curNum;
-                pre = char;
-                curNum = 1;
-            }
-        }
-        res += pre;
-        res += curNum;
-        if (res.length > str.length) {
-            console.warn('sc', str.length, res.length);
-        } else {
-            console.log('sc', str.length, res.length);
-        }
-        return res;
-    }
-
-    /**
-     * 解壓縮字串(支援多位長度數字,但原文不能包含數字)
-     * @param {string} str 壓縮後的字串
-     * @return {string} 原文
-     */
-    static decompressionEx(str: string): string {
-        let result: string = '';
-        const strArr: string[] = (str + 'X').split('');
-        let nowchar = '';
-        let nownum = '';
-        for (let i = 0; i < strArr.length; i++) {
-            const char: string = strArr[i];
-            const isNumber = !isNaN(parseInt(char));
-            if (isNumber) {
-                nownum += char;
-            } else {
-                if (nownum.length > 0) {
-                    for (let j = 0; j < parseInt(nownum); j++) {
-                        result += nowchar;
-                    }
-                    nownum = '';
-                }
-                nowchar = char;
-            }
-        }
-        return result;
-    }
-
-    /**
-     * 單元測試
-     * @param {string} str 要測試的原文字串
-     */
-    /*
-    static test(str: string) {
-        let code: string = str;
-        console.log('原文', str);
-        code = NyaCompressRLE.compression(code);
-        console.log('RLE 壓縮後', code);
-        code = NyaCompressRLE.decompression(code);
-        console.log('RLE 解壓後', code);
-        // 校對
-        if (str == code) {
-            console.log('校對成功');
-        } else {
-            console.error('校對失敗');
-        }
-    }
-    */
+    // /**
+    //  * 單元測試
+    //  * @param {string} str 要測試的原文字串
+    //  */
+    // static test(str: string) {
+    //     let code: string = str;
+    //     console.log('原文', str);
+    //     code = NyaCompressRLE.compression(code);
+    //     console.log('RLE 壓縮後', code);
+    //     code = NyaCompressRLE.decompression(code);
+    //     console.log('RLE 解壓後', code);
+    //     // 校對
+    //     if (str == code) {
+    //         console.log('校對成功');
+    //     } else {
+    //         console.error('校對失敗');
+    //     }
+    // }
 }
